bcd3266faf4b6bf24fac926c53d5d50082ca1979
Oscar Nolasco <oscarnolasco@mail.weber.edu>
2022-02-10 20:53:21 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2> {{ title }} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hello World'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
